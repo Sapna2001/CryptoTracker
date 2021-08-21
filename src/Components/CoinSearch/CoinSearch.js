bdc413617b26e21adb42a3ec52c1b3651d5f36a5
Sapna2001 <harisapnanair@gmail.com>
2021-08-21 22:09:07 +0530

    initial commit

---
+++ src/Components/CoinSearch/CoinSearch.js
@@ -0,0 +1,39 @@
+import React, { useState, createContext } from "react";
+import CoinTable from "../CoinTable/CoinTable";
+import "./CoinSearch.css";
+
+
+const coinPassed = createContext();
+
+function CoinSearch() {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+    // console.log(event.target.value);
+  };
+
+  return (
+    <>
+      <div className="coinSearchCointainer">
+        <div className="coinSearch">
+          <h1 className="coinText">Search a currency</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Search"
+              className="coinInput"
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+        <coinPassed.Provider value = {search}> 
+          <CoinTable/> 
+        </coinPassed.Provider> 
+      </div>
+    </>
+  );
+}
+
+export default CoinSearch;
+export { coinPassed };
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div class="footer">
+            <p>Made with ♥️ by <a href = "https://www.linkedin.com/in/sapna2001/" className = "link">Sapna2001</a></p>
+        </div>
+    )
+}
+
+export default Footer
