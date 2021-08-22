6c48e710ebf6d397a36e66a586a2aefc994aeaed
Sapna2001 <harisapnanair@gmail.com>
2021-08-22 11:17:24 +0530

    github added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
     return (
         <div class="footer">
             <p>Made with ♥️ by <a href = "https://www.linkedin.com/in/sapna2001/" className = "link">Sapna2001</a></p>
+            <p><a href="https://github.com/Sapna2001/WhatsAppMessenger"><i className = "fa fa-github fa-2x link"></i></a></p>
         </div>
     )
 }
